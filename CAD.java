9982a11191e884486c9c2bea3cf6093ba08c2c29
tonyRitrovato <115578428+tonyRitrovato@users.noreply.github.com>
2023-01-14 12:25:02 +0100

    first commit

---
+++ CAD.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class CAD extends JFrame{
+
+    public CAD() {
+        setSize (500, 420);
+        setUndecorated(true);
+        setLocation (50,50);
+        setResizable(false); 
+        setVisible(true);
+        //setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+    }
+}
