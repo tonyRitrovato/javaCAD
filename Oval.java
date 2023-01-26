a49cac2bdbc9b3eb136c5f61c3d85d238ccbe22a
Tony Ritrovato <antonioritrovato17@gmail.com>
2023-01-26 09:13:52 +0100

    oval fixed and selection totally implemented

--- Oval.java
+++ Oval.java
@@ -1,5 +1,6 @@
 
 import java.awt.*;
+import java.awt.geom.*;
 
 public class Oval extends Draw {
 
@@ -20,18 +21,21 @@ public class Oval extends Draw {
     }
 
     public void paintComponent(Graphics g) {
-         diffx = (int)(raggio.getX() - centro.getX() / 2);
-         diffy = (int)(raggio.getY() - centro.getY() / 2);
+         diffx = (int)((raggio.getX() - centro.getX()) / 2);
+         diffy = (int)((raggio.getY() - centro.getY()) / 2);
         super.paintComponent(g);
         if(fill == false)
-            g.drawOval((int)centro.getX(), (int)centro.getY(), diffx * 2, diffy * 2);
+            g.drawOval((int)(centro.getX() - diffx), (int)(centro.getY() - diffy),(int) diffx * 2 ,(int) diffy * 2);
         else
-            g.fillOval((int)centro.getX(), (int)centro.getY(), diffx * 2, diffy * 2);
+            g.fillOval((int)(centro.getX() - diffx), (int)(centro.getY() - diffy), (int) diffx * 2,(int) diffy * 2);
     }
 
-    public boolean intersects(Selection d) {
-       
-        return true;
-    }
-    
+    public boolean intersects(Selection sel) {
+       Ellipse2D e = new Ellipse2D.Double(centro.getX(), centro.getY(), diffx * 2, diffy * 2);
+        Rectangle2D s = new Rectangle2D.Double(sel.getPunto1().getX(), sel.getPunto1().getY(), sel.getPunto2().getX() - sel.getPunto1().getX(), sel.getPunto2().getY() - sel.getPunto1().getY());
+        if (e.intersects(s)) 
+            return true;
+        else
+            return false;
+    }    
 }
